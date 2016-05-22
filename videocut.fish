5c0de86a59bff4fa49885d9c39cfd6bd68a6d110
ayhankuru <ayhankuru@yandex.com.tr>
2016-05-22 17:04:54 +0300

    fix

--- videocut.fish
+++ videocut.fish
@@ -3,16 +3,16 @@
 #
 # USAGE
 #   Options
-#   Src videofile
-#   Start time
-#   Cut Time
+#   {src} Video file
+#   {start} Time
+#   {end} Time
+#   {type} video output type
 
 function videocut
-  if not available ffmpeg
-    echo "📂  Please install 'apt-get install ffmpeg ' first!"
-  else
-  	set rnumb  (eval random )
+  if type -t ffmpeg > /dev/null
+    set rnumb  (eval random )
     ffmpeg -i $argv[1] -vcodec copy -acodec copy -ss $argv[2] -to $argv[3] -strict -2 cut_$rnumb.$argv[4]  > /dev/null 2>&1
-    notify-send "video kırpma işlemi bitti"
+  else
+    echo "📂  Please install 'apt-get install ffmpeg ' first!"
   end
 end
